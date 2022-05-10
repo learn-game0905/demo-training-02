cec30be9543724393b1d2ae6c362f4784d717e9d
huy2012 <huynn@reactplus.jp>
2022-05-10 23:48:48 +0700

    learn c#

---
+++ Assets/scripts/hello.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string message = "Hello World!!";
+
+        Debug.Log(message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
