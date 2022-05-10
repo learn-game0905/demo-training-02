6a2283398847f817ec4b892a44cb42f3e0839c9f
huy2012 <huynn@reactplus.jp>
2022-05-11 00:12:18 +0700

    learn class

--- Assets/scripts/hello.cs
+++ Assets/scripts/hello.cs
@@ -10,11 +10,38 @@ public class hello : MonoBehaviour
         string message = "Hello World!!";
 
         Debug.Log(message);
+
+        MyClass newClass = new MyClass();
+        Debug.Log(newClass.MyMethod("mes1" , "mes 2"));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+}
+
+public class MyClass
+{
+    public string myField = string.Empty;
+
+    public MyClass()
+    {
+    }
+
+    public string MyMethod(string parameter1, string parameter2)
+    {
+        return "First Parameter " + parameter1 + ", second parameter " + parameter2;
+    }
+
+    public int MyAutoImplementedProperty { get; set; }
+
+    private int myPropertyVar;
+
+    public int MyProperty
+    {
+        get { return myPropertyVar; }
+        set { myPropertyVar = value; }
     }
 }
