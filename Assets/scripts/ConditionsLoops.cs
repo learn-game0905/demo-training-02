713d6e8f255a7fda4f2c9c7dfa2d81c27c8ad70a
huy2012 <huynn@reactplus.jp>
2022-05-12 14:02:31 +0700

    learn C# - Switch Statement

--- Assets/scripts/ConditionsLoops.cs
+++ Assets/scripts/ConditionsLoops.cs
@@ -8,6 +8,21 @@ public class ConditionsLoops : MonoBehaviour
     {
         return i > j;                    
     }
+    
+    static bool isOdd(int i, int j , int x)
+    {
+        switch (x % 2)
+        { 
+            case 0:
+                return true;
+            case 1:
+                return false;
+            default:
+                return false;
+        }
+    
+        return false;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -85,10 +100,109 @@ public class ConditionsLoops : MonoBehaviour
 
         Debug.Log(result);*/
         
-        var x = 2, y = 10;
+        /*var x = 2, y = 10;
 
         var result = x * 3 > y ? x : y > z? y : z;
-        Debug.Log(result);
+        Debug.Log(result);*/
+        
+        // C# - Switch Statement
+        /*int x = 10;
+
+        switch (x)
+        { 
+            case 5:
+                Debug.Log("Value of x is 5");
+                break;
+            case 10:
+                Debug.Log("Value of x is 10");
+                break;
+            case 15:
+                Debug.Log("Value of x is 15");
+                break;
+            default:
+                Debug.Log("Unknown value");
+                break;
+        }*/
+        
+        /*int x = 125;
+
+        switch (x % 2)
+        { 
+            case 0:
+                Debug.Log($"{x} is an even value");
+                break;
+            case 1:
+                Debug.Log($"{x} is an odd Value");
+                break;
+        }*/
+        
+        /*string statementType = "switch";
+
+        switch (statementType)
+        {
+            case "if.else":
+                Debug.Log("if...else statement");
+                break;
+            case "ternary":
+                Debug.Log("Ternary operator");
+                break;
+            case "switch":
+                Debug.Log("switch statement");
+                break;
+        }*/
+        
+        
+        /*int x = 5;
+
+        switch (x)
+        { 
+            case 1:
+                Debug.Log("x = 1");
+                break;
+            case 2:
+                Debug.Log("x = 2");
+                break;
+            case 4:
+            case 5:
+                Debug.Log("x = 4 or x = 5");
+                break;
+            default:
+                Debug.Log("x > 5");
+                break;
+        }*/
+        
+        // int x = 125;
+        // Debug.Log( isOdd(1,2,x)? "Even value" : "Odd value");
+        
+        /*int j = 5;
+
+        switch (j)
+        {
+            case 5:
+                Debug.Log(5);
+                switch (j - 1)
+                {
+                    case 4:
+                        Debug.Log(4);
+                        switch (j - 2)
+                        {
+                            case 3:
+                                Debug.Log(3);
+                                break;
+                        }
+                        break;
+                }
+                break;
+            case 10:
+                Debug.Log(10);
+                break;
+            case 15:
+                Debug.Log(15);
+                break;
+            default:
+                Debug.Log(100);
+                break;
+        }*/
     }
 
     // Update is called once per frame
