efd391cce33f50b53865eb7cdb02479c20c1579f
huy2012 <huynn@reactplus.jp>
2022-05-12 15:11:38 +0700

    learn C# - Multidimensional Arrays

--- Assets/scripts/Collections.cs
+++ Assets/scripts/Collections.cs
@@ -71,6 +71,87 @@ public class Collections : MonoBehaviour
         foreach(var item in nums)
             Debug.Log(item);*/
         
+        // C# - Multidimensional Arrays
+        /*int[,] arr2d; // two-dimensional array
+        int[, ,] arr3d; // three-dimensional array
+        int[, , ,] arr4d ; // four-dimensional array
+        int[, , , ,] arr5d; // five-dimensional array*/
+        
+        /*int[,] arr2d = new int[3,2]{ 
+            {1, 2}, 
+            {3, 4}, 
+            {5, 6} 
+        };
+        
+        int[,] arr2d = { 
+            {1, 2}, 
+            {3, 4}, 
+            {5, 6} 
+        };*/
+        
+        /*int[,] arr2d = new int[3,2]{ 
+            {1, 2}, 
+            {3, 4}, 
+            {5, 6} 
+        };*/
+
+        /*arr2d[0, 0]; //returns 1
+        arr2d[0, 1]; //returns 2
+        arr2d[1, 0]; //returns 3
+        arr2d[1, 1]; //returns 4
+        arr2d[2, 0]; //returns 5
+        arr2d[2, 1]; //returns 6*/
+        
+        
+        /*int[, ,] arr3d1 = new int[1, 2, 2]{
+            { { 1, 2}, { 3, 4} }
+        };
+
+        int[, ,] arr3d2 = new int[2, 2, 2]{
+            { {1, 2}, {3, 4} },
+            { {5, 6}, {7, 8} }
+        };
+
+        int[, ,] arr3d3 = new int[2, 2, 3]{
+            { { 1, 2, 3}, {4, 5, 6} },
+            { { 7, 8, 9}, {10, 11, 12} }
+        };
+
+        arr3d2[0, 0, 0]; // returns 1 
+        arr3d2[0, 0, 1]; // returns 2 
+        arr3d2[0, 1, 0]; // returns 3 
+        arr3d2[0, 1, 1]; // returns 4 
+        arr3d2[1, 0, 0]; // returns 5 
+        arr3d2[1, 0, 1]; // returns 6 
+        arr3d2[1, 1, 0]; // returns 7 
+        arr3d2[1, 1, 1]; // returns 8*/
+        
+        /*int[,,,] arr4d1 = new int[1, 1, 2, 2]{
+            { 
+                { { 1, 2}, { 3, 4} }     
+            }
+        };
+
+        arr4d1[0, 0, 0, 0]; // returns 1
+        arr4d1[0, 0, 0, 1]; // returns 2
+        arr4d1[0, 0, 1, 0]; // returns 3
+        arr4d1[0, 0, 1, 1]; // returns 4
+
+        int[,,,] arr4d2 = new int[1, 2, 2, 2]{
+            {
+                { {1, 2}, {3, 4} },
+                { {5, 6}, {7, 8} }
+            }
+        };
+
+        arr4d2[0, 0, 0, 0]; // returns 1
+        arr4d2[0, 0, 0, 1]; // returns 2
+        arr4d2[0, 0, 1, 0]; // returns 3
+        arr4d2[0, 0, 1, 1]; // returns 4
+        arr4d2[0, 1, 0, 0]; // returns 5
+        arr4d2[0, 1, 0, 1]; // returns 6
+        arr4d2[0, 1, 1, 0]; // returns 7
+        arr4d2[0, 1, 1, 1]; // returns 8*/
     }
 
     // Update is called once per frame
